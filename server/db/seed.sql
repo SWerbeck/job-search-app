f65d1f2973ea0856d6702beebdc6bfa1e11bd84a
Stephen Werbeck <SWerbeck@yahoo.com>
2024-01-31 19:36:44 -0500

    set up many to many association in seed file

--- server/db/seed.sql
+++ server/db/seed.sql
@@ -1,2 +1,30 @@
---Hello this is a comment from the file we want to read 
---is this happening on main????
+DROP TABLE IF EXISTS _AppliedCompany;
+
+CREATE TABLE _AppliedCompany(
+ ID INT PRIMARY KEY NOT NULL,
+ company_id int NOT NULL,
+ user_id int NOT NULL,
+ FOREIGN KEY (company_id) REFERENCES _COMPANY(ID)
+ ON DELETE CASCADE,
+ FOREIGN KEY (user_id) REFERENCES _USER(ID)
+ ON DELETE CASCADE
+
+
+
+);
+
+
+INSERT INTO _AppliedCompany(ID, company_id, user_id)
+VALUES (1, 1, 1);
+
+INSERT INTO _AppliedCompany(ID, company_id, user_id)
+VALUES (2, 3, 1);
+
+INSERT INTO _AppliedCompany(ID, company_id, user_id)
+VALUES (3, 7, 1);
+
+INSERT INTO _AppliedCompany(ID, company_id, user_id)
+VALUES (4, 2, 2);
+
+INSERT INTO _AppliedCompany(ID, company_id, user_id)
+VALUES (5, 6, 2);
