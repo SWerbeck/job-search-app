d16266939eccb7deeb0626057bb84d85293bd513
Stephen Werbeck <SWerbeck@yahoo.com>
2024-02-05 18:48:50 -0500

    set up uuid for companies and applied companies, switched select user id to where username

--- server/db/seed.sql
+++ server/db/seed.sql
@@ -1,13 +1,13 @@
 DROP TABLE IF EXISTS _AppliedCompany;
 
 CREATE TABLE _AppliedCompany(
- ID INT PRIMARY KEY NOT NULL,
- company_id int NOT NULL,
+ applied_id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+ company_id uuid NOT NULL,
  user_id uuid NOT NULL,
- FOREIGN KEY (company_id) REFERENCES _COMPANY(ID)
+ FOREIGN KEY (company_id) REFERENCES _COMPANY(company_id)
  ON DELETE CASCADE,
  FOREIGN KEY (user_id) REFERENCES _USER(user_id)
- --ON DELETE CASCADE
+ ON DELETE CASCADE
 );
 
 -- ALTER TABLE _AppliedCompany
@@ -18,18 +18,27 @@ CREATE TABLE _AppliedCompany(
 --    FOREIGN KEY (user_id) 
 --    REFERENCES _USER(user_id);
 
-INSERT INTO _AppliedCompany(ID, company_id, user_id)
-VALUES (1, 1, (SELECT user_id FROM _USER WHERE FIRST_NAME = 'Stephen'));
+INSERT INTO _AppliedCompany(company_id, user_id)
+VALUES ((SELECT company_id FROM _COMPANY WHERE COMPANYNAME = 'GOOGLE'), (SELECT user_id FROM _USER WHERE UserName = 'louisiscool123'));
 
+INSERT INTO _AppliedCompany(company_id, user_id)
+VALUES ((SELECT company_id FROM _COMPANY WHERE COMPANYNAME = 'Spotify'), (SELECT user_id FROM _USER WHERE UserName = 'louisiscool123'));
 
--- INSERT INTO _AppliedCompany(ID, company_id, user_id)
--- VALUES (2, 3, 1);
+INSERT INTO _AppliedCompany(company_id, user_id)
+VALUES ((SELECT company_id FROM _COMPANY WHERE COMPANYNAME = 'X'), (SELECT user_id FROM _USER WHERE UserName = 'louisiscool123'));
 
--- INSERT INTO _AppliedCompany(ID, company_id, user_id)
--- VALUES (3, 7, 1);
+INSERT INTO _AppliedCompany(company_id, user_id)
+VALUES ((SELECT company_id FROM _COMPANY WHERE COMPANYNAME = 'Data-Dog'), (SELECT user_id FROM _USER WHERE UserName = 'louisiscool123'));
 
--- INSERT INTO _AppliedCompany(ID, company_id, user_id)
--- VALUES (4, 2, 2);
+INSERT INTO _AppliedCompany(company_id, user_id)
+VALUES ((SELECT company_id FROM _COMPANY WHERE COMPANYNAME = 'Pinterest'), (SELECT user_id FROM _USER WHERE UserName = 'SWerb'));
 
--- INSERT INTO _AppliedCompany(ID, company_id, user_id)
--- VALUES (5, 6, 2);
+INSERT INTO _AppliedCompany(company_id, user_id)
+VALUES ((SELECT company_id FROM _COMPANY WHERE COMPANYNAME = 'Shopify'), (SELECT user_id FROM _USER WHERE UserName = 'SWerb'));
+
+INSERT INTO _AppliedCompany(company_id, user_id)
+VALUES ((SELECT company_id FROM _COMPANY WHERE COMPANYNAME = 'Rumble'), (SELECT user_id FROM _USER WHERE UserName = 'SWerb'));
+
+INSERT INTO _AppliedCompany(company_id, user_id)
+VALUES ((SELECT company_id FROM _COMPANY WHERE COMPANYNAME = 'GOOGLE'), (SELECT user_id FROM _USER WHERE UserName = 'SWerb'));
+
