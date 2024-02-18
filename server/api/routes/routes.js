efaa013e3b9a7d8a4ad5bbf028f5f2becf40806b
Louis J Rabeno <lrabeno@gmail.com>
2024-02-18 09:12:05 -0500

    company express route working

--- server/api/routes/routes.js
+++ server/api/routes/routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 module.exports = router;
 
 router.use('/users', require('./user'));
+router.use('/companies', require('./company'));
 
 router.use((req, res, next) => {
   const error = new Error('Not Found');
