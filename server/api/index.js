cbf2a843a6f2c718dd08c7b00e6a8d62b279ec13
Louis J Rabeno <lrabeno@gmail.com>
2024-04-08 09:50:02 -0400

    added cors and axios to package.json and can fetch data on front end

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,7 @@
-import express from "express";
-import router from "./routes/routes.js";
+import express from 'express';
+import router from './routes/routes.js';
+import cors from 'cors';
+
 const app = express();
 const port = 3000;
 
@@ -9,12 +11,13 @@ app.use(
     extended: true,
   })
 );
+app.use(cors());
 
-app.get("/", (req, res) => {
-  res.json({ info: "Node.js, Express, and Postgres API" });
+app.get('/', (req, res) => {
+  res.json({ info: 'Node.js, Express, and Postgres API' });
 });
 
-app.use("/api", router);
+app.use('/api', router);
 //app.get('/users', db.getUsers);
 
 app.listen(port, () => {
@@ -23,7 +26,6 @@ app.listen(port, () => {
 
 //seeds all 3 sql files
 //"seed":  "psql -d jsadb -a -f ./server/db/user.sql && psql -d jsadb -a -f ./server/db/company.sql && psql -d jsadb -a -f ./server/db/application.sql && psql -d jsadb -a -f ./server/db/contact.sql"
-
 
 // below seeds from seed.js
 //"seed":  "psql -d jsadb -a -f ./server/db/seed.js"
