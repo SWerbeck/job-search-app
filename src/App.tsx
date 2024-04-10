d5bfc4ca69c75d5e3d1c909bb033d396dc31520f
Louis J Rabeno <lrabeno@gmail.com>
2024-04-10 18:48:48 -0400

    display users in app component on front end

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,57 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const [users, setUsers] = useState([]);
+
   const fetchUsers = async () => {
     try {
       const fetchedUsers = await axios.get('http://localhost:3000/api/users');
-      console.log('from app file', fetchedUsers.data);
+      setUsers(fetchedUsers.data.users);
+      console.log(users, 'from usestate hook');
     } catch (err) {
       console.log(err);
     }
   };
 
+  const fetchCompanys = async () => {
+    try {
+      const fetchedCompanys = await axios.get(
+        'http://localhost:3000/api/companies'
+      );
+      //console.log('companies from app file', fetchedCompanys.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   useEffect(() => {
     fetchUsers();
+    fetchCompanys();
   }, []);
+
+  console.log(users, 'outside usestate hook');
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {users.map((user) => {
+          return (
+            <div key={user.user_id}>
+              <h1>{`${user.first_name + user.last_name}`}</h1>
+              <h3>{user.username}</h3>
+            </div>
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
