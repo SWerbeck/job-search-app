f0348acd4368fd9af176c58bca45f8e1d85a2ac0
Louis J Rabeno <lrabeno@gmail.com>
2025-01-26 11:14:37 -0500

    added application css and found single company bug

--- src/components/cards/EditApplicationCard.tsx
+++ src/components/cards/EditApplicationCard.tsx
@@ -13,95 +13,89 @@ import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useDisable } from '../../context/DisableContext';
 
-  
 // make a schema using zod
 const schema = z.object({
-    job_title: z.string().min(1, {
-      message: 'Please edit job title',
-    }),
-  });
-  
-  type FormFields = z.infer<typeof schema>;
+  job_title: z.string().min(1, {
+    message: 'Please edit job title',
+  }),
+});
 
+type FormFields = z.infer<typeof schema>;
 
-  const  EditApplicationCard = ( {
-    singleApp,
-    contactsList,
-    editMode,
-    setEditMode}) => {
-        
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const usersList = useSelector((state: RootState) => state.users.users);
-      
+const EditApplicationCard = ({
+  singleApp,
+  contactsList,
+  editMode,
+  setEditMode,
+}) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const usersList = useSelector((state: RootState) => state.users.users);
 
-        const {
-            register,
-            handleSubmit,
-            setError,
-            formState: { errors, isSubmitting },
-          } = useForm<FormFields>({ resolver: zodResolver(schema) });
-        
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm<FormFields>({ resolver: zodResolver(schema) });
 
-const appId = singleApp?.Application_ID
-const userId = usersList[0]?.user_id;
-const axiosPrivate = useAxiosPrivate();
-const { isDisabled, setIsDisabled } = useDisable();
+  const appId = singleApp?.Application_ID;
+  const userId = usersList[0]?.user_id;
+  const axiosPrivate = useAxiosPrivate();
+  const { isDisabled, setIsDisabled } = useDisable();
 
+  const [toastId, setToastId] = useState<string | number | null>(null);
 
-const [toastId, setToastId] = useState<string | number | null>(null);
-
-const handleConfirm = () => {
-  // Perform the action you want on confirmation
-  toast.dismiss(toastId);
-  deleteApplication(appId)
-  // Enable interactions again
+  const handleConfirm = () => {
+    // Perform the action you want on confirmation
+    toast.dismiss(toastId);
+    deleteApplication(appId);
+    // Enable interactions again
     console.log('Confirmed!');
-};
+  };
 
-const handleCancel = () => {
-  // Do nothing or handle the cancellation
-  toast.dismiss(toastId);
-  setToastId(null); // Enable interactions again
-  console.log('Cancelled!');
-  
-};
+  const handleCancel = () => {
+    // Do nothing or handle the cancellation
+    toast.dismiss(toastId);
+    setToastId(null); // Enable interactions again
+    console.log('Cancelled!');
+  };
 
-const showConfirmationToast = () => {
-  if (!toastId || !toast.isActive(toastId)) {
-    setIsDisabled(true)
-    const id = toast.info(
-      <div>
-        <p>Are you sure?</p>
-        <button onClick={handleConfirm}>Yes</button>
-        <button onClick={handleCancel}>No</button>
-      </div>,
-      {
-        autoClose: false,
-        closeOnClick: false,
-        draggable: false,
-        pauseOnHover: false,
-        onClose: () => setIsDisabled(false)
-      }
-    );
-    setToastId(id); // Correct type now matches
-  }
-};
+  const showConfirmationToast = () => {
+    if (!toastId || !toast.isActive(toastId)) {
+      setIsDisabled(true);
+      const id = toast.info(
+        <div>
+          <p>Are you sure?</p>
+          <button onClick={handleConfirm}>Yes</button>
+          <button onClick={handleCancel}>No</button>
+        </div>,
+        {
+          autoClose: false,
+          closeOnClick: false,
+          draggable: false,
+          pauseOnHover: false,
+          onClose: () => setIsDisabled(false),
+        }
+      );
+      setToastId(id); // Correct type now matches
+    }
+  };
 
-const deleteApplication = async (appId: string) => {
-  try {
-    const applicationToDelete = await axiosPrivate.delete(
-      `/api/applications/${appId}`
-    );
-    dispatch(deleteUserApps(appId));
-    setToastId(null); 
-    navigate(`/home/${userId}/applications`);
-  } catch (err) {
-    console.log(err.response.data);
-    setToastId(null); 
-    navigate('/', { state: { from: location }, replace: true });
-  }
-};
+  const deleteApplication = async (appId: string) => {
+    try {
+      const applicationToDelete = await axiosPrivate.delete(
+        `/api/applications/${appId}`
+      );
+      dispatch(deleteUserApps(appId));
+      setToastId(null);
+      navigate(`/home/${userId}/applications`);
+    } catch (err) {
+      console.log(err.response.data);
+      setToastId(null);
+      navigate('/', { state: { from: location }, replace: true });
+    }
+  };
 
   const onSubmit: SubmitHandler<FormFields> = async (data) => {
     try {
@@ -119,45 +113,48 @@ const deleteApplication = async (appId: string) => {
     }
   };
 
+  console.log('duuuun', singleApp);
 
   return (
     <div>
-        {toastId ? <button
-        disabled
-        className="bg-button3 text-mainbody"
-      >
-        Cancel
-      </button> : <button
-        onClick={() => setEditMode(false)}
-        className="bg-button3 text-mainbody"
-      >
-        Cancel
-      </button>}
-      <button className="bg-button1 text-white"
-        onClick={showConfirmationToast} 
-        // deleteApplication(appId)
-        disabled={isSubmitting}>
-          {isSubmitting ? 'Loading...' : 'Delete Application'}
+      {toastId ? (
+        <button disabled className="bg-button3 text-mainbody">
+          Cancel
         </button>
-        
-        
-      
+      ) : (
+        <button
+          onClick={() => setEditMode(false)}
+          className="bg-button3 text-mainbody"
+        >
+          Cancel
+        </button>
+      )}
+      <button
+        className="bg-button1 text-white"
+        onClick={showConfirmationToast}
+        // deleteApplication(appId)
+        disabled={isSubmitting}
+      >
+        {isSubmitting ? 'Loading...' : 'Delete Application'}
+      </button>
+
       <form onSubmit={handleSubmit(onSubmit)}>
         <input
           {...register('job_title')}
           type="text"
           placeholder={singleApp?.Position}
         />
-        {toastId ? <button className="bg-button2 text-white" disabled>
-          Submit
-        </button> :  <button className="bg-button2 text-white" disabled={isSubmitting}>
-          {isSubmitting ? 'Loading...' : 'Submit'}
-        </button>}
-       
-        
+        {toastId ? (
+          <button className="bg-button2 text-white" disabled>
+            Submit
+          </button>
+        ) : (
+          <button className="bg-button2 text-white" disabled={isSubmitting}>
+            {isSubmitting ? 'Loading...' : 'Submit'}
+          </button>
+        )}
       </form>
-      
     </div>
-  )
-}
-export default EditApplicationCard
+  );
+};
+export default EditApplicationCard;
