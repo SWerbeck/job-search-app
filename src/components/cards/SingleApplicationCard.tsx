290c128aa0487e7dfb7b0494afb04f551736a71c
Louis J Rabeno <lrabeno@gmail.com>
2025-02-02 11:42:50 -0500

    delete button working and single app displays

--- src/components/cards/SingleApplicationCard.tsx
+++ src/components/cards/SingleApplicationCard.tsx
@@ -24,6 +24,10 @@ const SingleApplicationCard = ({
     (state: RootState) => state.userApps.userApps
   );
 
+  // DATE FORMATTING
+  const appliedDate = new Date(singleApp?.Applied_Date);
+  const lastUpdatedDate = new Date(singleApp?.Last_Updated_Date);
+
   // useEffect(() => {
   //   console.log('useEffect triggered fromt he form');
   // }, [userApplications]);
@@ -32,8 +36,8 @@ const SingleApplicationCard = ({
     <p key={contact.CONTACT_ID}>{contact?.CONTACT_NAME}</p>
   ));
 
-  console.log('contactsList from card', contactsList);
-  console.log('mapped contacts in card', contactsMap);
+  //console.log('contactsList from card', contactsList);
+  //console.log('mapped contacts in card', contactsMap);
   return (
     <div className="flex justify-center items-center">
       <button
@@ -42,11 +46,59 @@ const SingleApplicationCard = ({
       >
         Edit
       </button>
-
-      {/* <h1>single Application card</h1> */}
       <div className="m-10">
         <p className="text-lg">Company: {singleApp?.company_name}</p>
         <p className="text-sm">Job Title: {singleApp?.Position}</p>
+        <p className="text-sm">
+          Website:{' '}
+          {singleApp?.Company_Website
+            ? singleApp?.Company_Website
+            : 'No website plz add'}{' '}
+          {singleApp?.Company_Website}
+        </p>
+        <p className="text-sm">Status: {singleApp?.Status}</p>
+        <div className="flex items-center">
+          <p className="text-sm">
+            Applied Date:{' '}
+            {appliedDate.toLocaleString('en-US', {
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric',
+            })}
+          </p>
+          <p className="text-xs pl-1">
+            {appliedDate.toLocaleString('en-US', {
+              hour: '2-digit',
+              minute: '2-digit',
+              second: '2-digit',
+              timeZoneName: 'short',
+            })}
+          </p>
+        </div>
+        <div className="flex justify-center items-center">
+          <p className="text-sm">
+            Last updated date:{' '}
+            {lastUpdatedDate.toLocaleString('en-US', {
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric',
+            })}
+          </p>
+          <p className="text-xs pl-1">
+            {lastUpdatedDate.toLocaleString('en-US', {
+              hour: '2-digit',
+              minute: '2-digit',
+              second: '2-digit',
+              timeZoneName: 'short',
+            })}
+          </p>
+        </div>
+        <p className="text-sm">
+          Application Info:{' '}
+          {singleApp?.application_info
+            ? singleApp?.application_info
+            : 'No info plz update'}
+        </p>
         <div>
           Contact List:{' '}
           {contactsMap.length > 0 ? contactsMap : <p>No Contacts</p>}
