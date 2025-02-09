582ded1a4c93329dba8183fbd456572eae528775
Stephen Werbeck <SWerbeck@yahoo.com>
2025-02-09 11:15:24 -0500

    redesigned applications css

--- src/components/Applicationscomponent.tsx
+++ src/components/Applicationscomponent.tsx
@@ -37,26 +37,30 @@ const Applications = () => {
   console.log('FROM ALL APPS', userApplications);
 
   const applicationData = userApplications?.map((userApp) => (
+    
+
     <div key={userApp?.company_id}>
-      <ul className="bg-button2 text-white h-10 w-50 border-spacing-x-7 border mx-3 border-white grid place-content-left content-center text-4xl p-10 mt-10">
+      <ul className=" bg-headline text-white h-10 w-50 border-spacing-x-7 border mx-3 border-white grid place-content-left content-center text-4xl p-10 mt-10 rounded-xl"
+      >
         {userApp?.company}
       </ul>
 
+    <div className='grid grid-cols-3 mx-3 mt-10 gap-x-4 gap-y-8'>
       {userApp?.applications?.map((app) => {
         const appliedDate = new Date(app?.Applied_Date); // Create a single Date instance
 
         return (
           <div
             key={app?.Application_ID}
-            className="bg-mainbody h-20 w-50 border-spacing-x-5 border mx-3 border-white grid place-content-center text-button3 text-4xl p-20"
+            className="bg-gray-100 border-spacing-x-5 mx-3 place-content-center text-4xl p-20 drop-shadow-lg rounded-xl"
           >
-            <Link to={`${location.pathname}/${app.Application_ID}`}>
               <div className="flex justify-center items-center pb-2">
-                <p>Job title: {app?.Position}</p>
+                {/* Job Title */}
+                <p>{app?.Position}</p>
               </div>
               <div className="flex justify-center items-center pb-4">
                 <p className="text-lg">
-                  Date applied:{' '}
+                  {/* Date applied*/}
                   {appliedDate.toLocaleString('en-US', {
                     year: 'numeric',
                     month: 'long',
@@ -72,7 +76,7 @@ const Applications = () => {
                   })}
                 </p>
               </div>
-            </Link>{' '}
+            
             <div className="flex justify-center items-center">
               <Link to={`${location.pathname}/${app.Application_ID}`}>
                 <button className="bg-button1 text-white text-base p-2">
@@ -80,9 +84,10 @@ const Applications = () => {
                 </button>
               </Link>
             </div>
-          </div>
+            </div>
         );
       })}
+    </div>
     </div>
   ));
 
@@ -106,13 +111,13 @@ const Applications = () => {
       > */}
         <button className="bg-button1 text-white">Add new Application</button>
       </Link>
-      <div className="bg-headline h-20 w-50 border-spacing-x-5 border border: mx-3 border-white grid place-content-center text-white text-4xl mt-8">
-        APPLICATIONS :
+      <div className="h-20 w-50 border-spacing-x-5 border border: mx-3 border-white grid place-content-center text-4xl mt-8">
+        APPLICATIONS
       </div>
       {applicationData}
-      {userApplications.map((userApp) => {
+      {/* {userApplications.map((userApp) => {
         return <div key={userApp?.company_id}></div>;
-      })}
+      })} */}
     </div>
   );
 };
