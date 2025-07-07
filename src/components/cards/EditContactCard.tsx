751002099cd3977117d3c04fe471360755811a77
Louis Rabeno <lrabeno@gmail.com>
2025-07-07 16:58:52 -0400

    styled edit contact page and fixed toast error

--- src/components/cards/EditContactCard.tsx
+++ src/components/cards/EditContactCard.tsx
@@ -10,7 +10,7 @@ const allowedOptions = ["has not replied", "replied"] as const;
 // make a schema using zod
 const schema = z.object({
   contactName: z.string().min(1, {
-    message: "Please edit the contacts name",
+    message: "Names can not be blank",
   }),
   linkedin: z
     .string()
@@ -97,65 +97,136 @@ const EditContactCard = ({
 
   //   console.log('edit mode from editCompanycard', editMode);
   return (
-    <>
-      <button
-        onClick={() => setEditMode(false)}
-        className="bg-button2 text-mainbody"
-      >
-        Cancel
-      </button>
-      <h1>edit mode</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          {...register("contactName")}
-          type="text"
-          placeholder={filteredContact[0].contactname}
-          defaultValue={filteredContact[0].contactname}
-        />
-        <input
-          {...register("linkedin")}
-          type="text"
-          placeholder={filteredContact[0].contact_linkedin}
-          //   defaultValue={filteredContact[0].contact_linkedin}
-        />
-        {errors.linkedin && (
-          <div className="flex justify-center items-center">
-            <p className="text-xs text-button4">{errors.linkedin.message}</p>
-          </div>
-        )}
-        <input
-          {...register("phone")}
-          type="text"
-          placeholder={filteredContact[0].contact_phone}
-        />
-        <input
-          {...register("email")}
-          type="text"
-          placeholder={filteredContact[0].contact_email}
-        />
-        {errors.email && (
-          <div className="flex justify-center items-center">
-            <p className="text-xs text-button4">{errors.email.message}</p>
-          </div>
-        )}
-        <select {...register("repliedSelectOption")}>
-          {allowedOptions.map((option) => (
-            <option key={option} value={option}>
-              {option}
-            </option>
-          ))}
-        </select>
-        {errors.repliedSelectOption && (
-          <span>{errors.repliedSelectOption.message}</span>
-        )}
-        <button
-          disabled={isSubmitting}
-          className="bg-button2 text-white px-4 py-2 rounded-md w-40 mb-10"
-        >
-          {isSubmitting ? "Loading..." : "Submit"}
-        </button>
-      </form>
-    </>
+    <div className="grid place-items-center mt-16 mb-20">
+      <div className="bg-gray-100 drop-shadow-lg rounded-xl xs:w-full sm:w-full md:w-3/4 lg:w-1/2">
+        <div className="bg-button2 rounded-tl-xl rounded-tr-xl rounded-br-sm rounded-bl-sm inset-x-0 top-0 flex justify-center items-center drop-shadow-lg mb-10">
+          <h3 className="text-xl text-white font-semibold mb-8 pt-5">
+            Edit contact for {filteredContact[0].contactname}
+          </h3>
+        </div>
+
+        <div className="flex flex-col items-center justify-center text-center">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col space-y-4"
+          >
+            <div className="flex items-center justify-center">
+              <label className="text-gray-700 text-md font-bold w-32 text-center">
+                Name:
+              </label>
+              <div className="flex flex-col w-64">
+                <input
+                  {...register("contactName")}
+                  type="text"
+                  className="bg-mainbody p-2 border rounded-md w-64"
+                  placeholder={filteredContact[0].contactname}
+                  defaultValue={filteredContact[0].contactname}
+                />
+                {errors.contactName && (
+                  <div className="flex justify-center items-center">
+                    <p className="text-red-500 text-xs">
+                      {errors.contactName.message}
+                    </p>
+                  </div>
+                )}
+              </div>
+            </div>
+
+            <div className="flex items-center justify-center">
+              <label className="text-gray-700 text-md font-bold w-32 text-center">
+                Linkedin url:
+              </label>
+              <div className="flex flex-col w-64">
+                <input
+                  {...register("linkedin")}
+                  type="text"
+                  className="bg-mainbody p-2 border rounded-md w-64"
+                  placeholder={filteredContact[0].contact_linkedin}
+                  defaultValue={filteredContact[0].contact_linkedin}
+                />
+                {errors.linkedin && (
+                  <div className="flex justify-center items-center">
+                    <p className="text-red-500 text-xs">
+                      {errors.linkedin.message}
+                    </p>
+                  </div>
+                )}
+              </div>
+            </div>
+
+            <div className="flex items-center justify-center">
+              <label className="text-gray-700 text-md font-bold w-32 text-center">
+                Contact phone:
+              </label>
+              <div className="flex flex-col w-64">
+                <input
+                  {...register("phone")}
+                  type="text"
+                  className="bg-mainbody p-2 border rounded-md w-64"
+                  placeholder={filteredContact[0].contact_phone}
+                />
+              </div>
+            </div>
+            <div className="flex items-center justify-center">
+              <label className="text-gray-700 text-md font-bold w-32 text-center">
+                Contact email:
+              </label>
+              <div className="flex flex-col w-64">
+                <input
+                  {...register("email")}
+                  type="text"
+                  className="bg-mainbody p-2 border rounded-md w-64"
+                  placeholder={filteredContact[0].contact_email}
+                  defaultValue={filteredContact[0].contact_email}
+                />
+                {errors.email && (
+                  <div className="flex justify-center items-center">
+                    <p className="text-red-500 text-xs">
+                      {errors.email.message}
+                    </p>
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className="flex items-center justify-center">
+              <label className="text-gray-700 text-md font-bold w-32 text-center">
+                Status:
+              </label>
+              <div className="flex flex-col w-64">
+                <select
+                  {...register("repliedSelectOption")}
+                  className="p-2 border rounded-md w-64 bg-mainbody"
+                >
+                  {allowedOptions.map((option) => (
+                    <option key={option} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+                {errors.repliedSelectOption && (
+                  <span>{errors.repliedSelectOption.message}</span>
+                )}
+              </div>
+            </div>
+
+            <div className="flex justify-center pt-5">
+              <button
+                disabled={isSubmitting}
+                className="bg-button2 text-white px-4 py-2 rounded-md w-40 mb-10"
+              >
+                {isSubmitting ? "Loading..." : "Submit"}
+              </button>
+              <button
+                onClick={() => setEditMode(false)}
+                className="bg-button2  text-white px-6 py-2 rounded-md w-40 text-center mb-10 mx-4"
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
 
